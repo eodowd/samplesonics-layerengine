15272ffd472c45a6bd6690e191a61c8c9a1d8ab2
opensamplist.com <46668191+eodowd@users.noreply.github.com>
2023-08-06 20:39:10 +0100

    updated

---
+++ Scripts/ScriptProcessors/le-new-back/Interface.js
@@ -0,0 +1,52 @@
+Content.makeFrontInterface(1024, 600);
+
+const var NUM_BUTTONS = 6;
+const var buttons = [];
+const var panels = [];
+
+for (i = 0; i < NUM_BUTTONS; i++)
+{
+    buttons[i] = Content.getComponent("Button" + (i));
+    panels[i] = Content.getComponent("Panel" + (i));
+    buttons[i].setControlCallback(onButtonControl);
+}
+
+inline function onButtonControl(component, value)
+{
+	local idx = buttons.indexOf(component);
+		
+	for (i = 0; i < panels.length; i++)
+    {
+        panels[i].showControl(idx == i);
+        buttons[i].setValue(i == idx && value);
+    }
+	
+}
+
+
+
+onButtonControl(buttons[0], true);
+
+
+Engine.loadFontAs("{PROJECT_FOLDER}fonts/Heebo-Light.ttf", "Heebo-Light");
+Engine.setGlobalFont("Heebo-Light");function onNoteOn()
+{
+	
+}
+ function onNoteOff()
+{
+	
+}
+ function onController()
+{
+	
+}
+ function onTimer()
+{
+	
+}
+ function onControl(number, value)
+{
+	
+}
+ 
