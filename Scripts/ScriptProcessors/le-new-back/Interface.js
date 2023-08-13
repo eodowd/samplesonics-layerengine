6b3af89b73d2314ec702ac228be1c6820e2e5f40
opensamplist.com <46668191+eodowd@users.noreply.github.com>
2023-08-14 00:16:18 +0100

    hello

--- Scripts/ScriptProcessors/le-new-back/Interface.js
+++ Scripts/ScriptProcessors/le-new-back/Interface.js
@@ -1,5 +1,10 @@
 Content.makeFrontInterface(1024, 600);
 
+const var ScriptAudioWaveform3 = Content.getComponent("ScriptAudioWaveform3");
+Console.print(ScriptAudioWaveform3.getRangeEnd());
+
+const var ScriptFX1 = Synth.getAudioSampleProcessor("Script FX1");
+Console.print(ScriptFX1.getSampleLength());
 const var NUM_BUTTONS = 6;
 const var buttons = [];
 const var panels = [];
@@ -29,7 +34,48 @@ onButtonControl(buttons[0], true);
 
 
 Engine.loadFontAs("{PROJECT_FOLDER}fonts/Heebo-Light.ttf", "Heebo-Light");
-Engine.setGlobalFont("Heebo-Light");function onNoteOn()
+Engine.setGlobalFont("Heebo-Light");
+
+
+
+
+
+inline function onScriptAudioWaveform3Control(component, value)
+{
+	Engine.allNotesOff();
+	
+
+	
+	
+};
+
+Content.getComponent("ScriptAudioWaveform3").setControlCallback(onScriptAudioWaveform3Control);
+
+
+inline function onScriptAudioWaveform4Control(component, value)
+{
+	Engine.allNotesOff();
+};
+
+Content.getComponent("ScriptAudioWaveform4").setControlCallback(onScriptAudioWaveform4Control);
+
+
+inline function onScriptAudioWaveform2Control(component, value)
+{
+	Engine.allNotesOff();
+};
+
+Content.getComponent("ScriptAudioWaveform2").setControlCallback(onScriptAudioWaveform2Control);
+
+
+inline function onScriptAudioWaveform2Control(component, value)
+{
+	Engine.allNotesOff();
+};
+
+Content.getComponent("ScriptAudioWaveform2").setControlCallback(onScriptAudioWaveform2Control);
+
+function onNoteOn()
 {
 	
 }
